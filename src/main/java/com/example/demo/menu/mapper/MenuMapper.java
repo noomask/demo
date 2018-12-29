04b3e14cabf3c4ec07d5c2239ff9d3dbcf518d33
ANNION <ANNION@DESKTOP-OFHQ4JQ>
2018-12-29 18:49:06 +0800

    结构调整

--- src/main/java/com/example/demo/menu/mapper/MenuMapper.java
+++ src/main/java/com/example/demo/menu/mapper/MenuMapper.java
@@ -3,7 +3,6 @@ package com.example.demo.menu.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -18,10 +17,6 @@ public interface MenuMapper {
 	@Select("SELECT * FROM t_menu WHERE id = #{id}")
 	Menu selectById(@Param("id")int id);
 	
-	@Insert("INSERT INTO t_menu ("
-			+ "  pid,name,menu_url,icon,is_show,view_order,add_user,add_date"
-			+ ")VALUES("
-			+ "  #{pid},#{name},#{menuUrl},#{icon},#{isShow},#{viewOrder},#{addUser},now())")
 	void insert(Menu menu);
 	
 	@Update("UPDATE t_menu SET name = #{name} WHERE id = #{id}")
